9a0deee812d329db2546abc9610b6b3590f66184
sf966gpm <mihailov.box@mail.ru>
2023-12-17 18:23:21 +0700

    feat: Добавил авторизацию и sanctum.

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Services\UserService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginUserRequest $request, UserService $userService): JsonResponse
+    {
+        $validated = $request->validated();
+
+        if (!Auth::attempt($validated)) {
+            return $this->error('', 'Не совпадают данные учетной записи.', 401);
+        }
+
+        return $this->success($userService->loginUserData($validated['email']));
+    }
+
+    public function register(StoreUserRequest $request, UserService $userService): JsonResponse
+    {
+        $validated = $request->validated();
+        return $this->success($userService->registerUserData($validated));
+    }
+
+    public function logout(): JsonResponse
+    {
+        $tokenCount = Auth::user()->tokens()->where('tokenable_id', Auth::id())->delete();
+
+        return $this->success('', 'Вы успешно вышли из системы. ' .
+            'Токенов было удалено - ' . $tokenCount . ' шт.');
+    }
+}
---
+++ src/app/Services/UserService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    /**
+     * Создаем пользователя
+     * @param array $validated
+     * @return array
+     */
+    public function registerUserData(array $validated): array
+    {
+        $user = User::create([
+            'user_roles_id' => $validated['user_roles_id'],
+            'email' => $validated['email'],
+            'name' => $validated['name'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return $this->userData($user);
+
+    }
+
+    /**
+     * Отдаём массив userData с данными.
+     * @param string $email
+     * Проверен на существование в базе
+     * @return array
+     */
+    public function loginUserData(string $email): array
+    {
+        $user = User::where(['email' => $email])->first();
+        return $this->userData($user);
+
+    }
+
+    /**
+     * Делает выборку данных у user
+     * @param User $user
+     * @return array
+     */
+    private function userData(User $user): array
+    {
+        return [
+            'user' => $user,
+            'token' => $user->createToken('API Token для ' . $user->email)->plainTextToken,
+            'token_type' => 'Bearer'
+        ];
+    }
+}
